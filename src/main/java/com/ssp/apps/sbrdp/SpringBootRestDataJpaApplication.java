1ee1a3006a1c379a3fdc8820e8363954804069a6
SivaprasadS <Somarouthu.sivaprasads@gmail.com>
2020-04-22 18:22:45 +0530

    Basic Project Setup

---
+++ src/main/java/com/ssp/apps/sbrdp/SpringBootRestDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ssp.apps.sbrdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestDataJpaApplication.class, args);
+    }
+
+}
