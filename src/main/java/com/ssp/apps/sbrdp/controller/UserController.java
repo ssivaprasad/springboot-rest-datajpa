45c370ab5df72ebab0b50982b96f638c8470e585
SivaprasadS <Somarouthu.sivaprasads@gmail.com>
2020-04-22 19:01:45 +0530

    Basic Crud Operations: Dummy Implementation

---
+++ src/main/java/com/ssp/apps/sbrdp/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.ssp.apps.sbrdp.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping
+    public String createUser() {
+        return "Create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "Update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "Delete user was called";
+    }
+
+    @GetMapping
+    public String getUsers() {
+        return "Get All users was called";
+    }
+
+
+}
