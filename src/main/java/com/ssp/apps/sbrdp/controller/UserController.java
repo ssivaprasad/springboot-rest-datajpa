a7a73066daf2d5c7db7e17f966b8edc418f4dc51
SivaprasadS <Somarouthu.sivaprasads@gmail.com>
2020-04-22 19:06:11 +0530

    Reading Path variables with @PathVariable

--- src/main/java/com/ssp/apps/sbrdp/controller/UserController.java
+++ src/main/java/com/ssp/apps/sbrdp/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ssp.apps.sbrdp.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,9 @@ public class UserController {
         return "Get All users was called";
     }
 
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable String id) {
+        return "Get user By Id was called : " + id;
+    }
 
 }
