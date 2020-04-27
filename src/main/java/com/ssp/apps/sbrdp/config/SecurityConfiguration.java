4c8430a44da4ee29d7f684681831b418354e1ab6
Sivaprasad_Somarouthu <sivaprasad_somarouthu@epam.com>
2020-04-27 19:00:25 +0530

    Enable InMemory Authentication

--- src/main/java/com/ssp/apps/sbrdp/config/SecurityConfiguration.java
+++ src/main/java/com/ssp/apps/sbrdp/config/SecurityConfiguration.java
@@ -1,8 +1,29 @@
 package com.ssp.apps.sbrdp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+
+    // @formatter:off
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .withUser("siva").password("siva").roles("USER").and()
+        .withUser("prasad").password("prasad").roles("ADMIN");
+    }
+    // @formatter:on
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
