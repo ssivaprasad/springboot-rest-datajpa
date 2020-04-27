1e5c2ae559b3dd1ca8a095d234795dec8a20a219
Sivaprasad_Somarouthu <sivaprasad_somarouthu@epam.com>
2020-04-27 19:36:27 +0530

    comment spring.datasource.initialization-mode infavor of ddl-auto Same time both spring.jpa.hibernate.ddl-auto and schema.sql can't be used

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into employee_dtl (id, name, email) values ('1478ef23-16a3-4265-83d7-92076fa08ef2', 'siva', 'dummy1@nodomain.com');
+insert into employee_dtl (id, name, email) values ('c9ac1e2c-cfdd-4719-ba94-c126d57dc035', 'prasad', 'dummy2@nodomain.com');
+insert into employee_dtl (id, name, email) values ('5aa45a97-f363-490f-959d-43b633913dd6', 'somarouthu', 'dummy3@nodomain.com');
