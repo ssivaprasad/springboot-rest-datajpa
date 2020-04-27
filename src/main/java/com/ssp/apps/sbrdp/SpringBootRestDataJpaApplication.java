6f3c8fc0b473d3b3c2983436d12cba7c787a370f
Sivaprasad_Somarouthu <sivaprasad_somarouthu@epam.com>
2020-04-27 18:49:38 +0530

    Enable Spring Security

--- src/main/java/com/ssp/apps/sbrdp/SpringBootRestDataJpaApplication.java
+++ src/main/java/com/ssp/apps/sbrdp/SpringBootRestDataJpaApplication.java
@@ -2,10 +2,8 @@ package com.ssp.apps.sbrdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class SpringBootRestDataJpaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ssp/apps/sbrdp/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.ssp.apps.sbrdp.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+}
