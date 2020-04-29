6eeb949ec37ad59d1afd246e6d6704e3277ce78e
Sivaprasad_Somarouthu <sivaprasad_somarouthu@epam.com>
2020-04-29 11:26:31 +0530

    disable csrf and permit every request without security

--- src/test/java/com/ssp/apps/sbrdp/config/SecurityTestConfiguration.java
+++ src/test/java/com/ssp/apps/sbrdp/config/SecurityTestConfiguration.java
@@ -12,6 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityTestConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 }
