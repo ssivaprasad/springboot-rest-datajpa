49ab10cdf962752bb1234fb52a67232c429a809e
Sivaprasad_Somarouthu <sivaprasad_somarouthu@epam.com>
2020-04-29 14:23:04 +0530

    Fix Compilation issue

--- src/main/java/com/ssp/apps/sbrdp/controller/AdminController.java
+++ src/main/java/com/ssp/apps/sbrdp/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.ssp.apps.spring.controller;
+package com.ssp.apps.sbrdp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
